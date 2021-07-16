b6d1fdca5b1149fc4683ceed956ab15c1d5cc275
chengxu.han <100873808@qq.com>
2021-07-16 17:00:28 +0900

    httpclient

--- hamster-esp32.ino
+++ hamster-esp32.ino
@@ -10,7 +10,9 @@
 #include "DHT.h"
 #include "word.h"
 #include "config.h"
-
+#include "esp_adc_cal.h"
+#include "medianFilter.h"
+#include "util.h"
 
 //引脚定义
 #define DHTPIN 14 //dht11 温湿度读取引脚
@@ -18,7 +20,9 @@
 
 #define OLED_SDA 21
 #define OLED_SCL 22
-#define LM35PIN 36 //lm35 温度传感器引脚
+#define LM35PIN 36 //lm35 温度传感器引脚 模拟值
+#define WEIGHT_PIN 34 //食盆儿重量引脚 模拟值
+
 #define SERVOPIN 18//喂食舵机引脚
 #define SpeadPonit 4 //读取跑圈数字接口
 
@@ -31,20 +35,16 @@ WiFiClient espClient;//wifi对象
 PubSubClient client(espClient);//mqtt对象
 Servo servo1;//喂食舵机
 Adafruit_SSD1306 display(128, 64, &Wire, -1);//oled显示屏
-
+medianFilter Filter;
 
 int pos = 0;//喂食舵机初始位置
 
 //---------------计算速度全局变量-----------------------
 int lastSts = 1; //default 1
 long lapCount = 0;//总圈数★
-long singleLapCount = 0; //单次运动圈数
 float totalRun = 0.0; //总里程★
 float tempTime1 = 0.0; // 长期处于识别区超时
 float endTime = 0; //用于计算平均速度
-float notRunTime = 0; //用于识别处于未转动状态，超过1 秒则结束一次平均速度计算
-boolean calSpeedFlg = false;//计算平均速度flg
-float singleSpeed = 0.0; //实时平均速度★
 //---------------计算速度全局变量-----------------------
 
 
@@ -54,33 +54,37 @@ void callback(String topic, byte* payload, unsigned int length) {
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);   
+    Serial.print((char)payload[i]);
   }
   Serial.println();
 
   //自动喂食任务
-  if(topic.equals("feedFood")){
-      StaticJsonDocument<200> doc;
-      DeserializationError error = deserializeJson(doc, payload);
-      
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
-      }
-    
-      int feedRunTime = doc["runTime"];
-      
-      digitalWrite(FEED_ONE, HIGH);
-      digitalWrite(FEED_TWO, LOW);
-      delay(1000);
-  
+  if (topic.equals("feedFood")) {
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    int feedRunTime = doc["runTime"];
+
+    int i = 0;
+    for (i = 0; i < feedRunTime; i++) {
+
       digitalWrite(FEED_ONE, LOW);
       digitalWrite(FEED_TWO, HIGH);
-      delay(1000*feedRunTime);
-  
+      delay(1000);
+
       digitalWrite(FEED_ONE, HIGH);
-      digitalWrite(FEED_TWO, HIGH);
+      digitalWrite(FEED_TWO, LOW);
+      delay(500);
+    }
+
+    digitalWrite(FEED_ONE, HIGH);
+    digitalWrite(FEED_TWO, HIGH);
   }
 }
 
@@ -95,7 +99,6 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-//    client.publish("outTopic", "hello world");
       //订阅喂食
       client.subscribe("feedFood");
     } else {
@@ -108,110 +111,130 @@ void reconnect() {
   }
 }
 
-//taskOne：每一秒循环
-void taskOne(void *parameter){
+//taskOne：每一秒循环 屏幕显示
+void taskOne(void *parameter) {
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);//开像素点发光
-  
-  while(1){
+
+  while (1) {
 
     display.clearDisplay();//清屏
-    display.drawLine(0,11,128,11,WHITE);
-    
+    display.drawLine(0, 11, 128, 11, WHITE);
+
     if ((WiFi.status() == WL_CONNECTED)) {
       display.drawBitmap(0, 0, wifiIcon, 10, 10, WHITE);
       //mqtt重连
       if (!client.connected()) {
         reconnect();
         vTaskDelay(1000);
-      }else{
-        display.drawBitmap(14, 0, mqIcon, 10, 10, WHITE); 
+      } else {
+        display.drawBitmap(14, 0, mqIcon, 10, 10, WHITE);
       }
       client.loop();
-    }else{
+    } else {
       //wifi重连
       WiFi.begin(ssid, password);
       while (WiFi.status() != WL_CONNECTED) {
         vTaskDelay(1000);
       }
     }
-  
+
     //环境温度
-    display.setCursor(1,15);
+    display.setCursor(1, 15);
     display.print("T:");
-    display.setCursor(15,15);
+    display.setCursor(15, 15);
     int t = dht.readTemperature();
-    if(t <100){
+    if (t < 100) {
       char t_char[6];
       dtostrf(t, 2, 0, t_char);
       display.print(t_char);
-    }else{
-      display.print(20);
+    } else {
+      display.print("--");
     }
-    
-    //加热垫温度
-    display.setCursor(25, 15);
-    display.print("|");
-    int temperature = analogRead(LM35PIN);
-    float voltage = temperature * (5.0 / 4096.0);
-    int temp = voltage*100;
-    display.setCursor(30, 15);
-    display.print(String(temp));
 
-    display.setCursor(1,25);
+    //加热垫温度
+    display.setCursor(30, 15);
+    display.print("(");
+    float LM35_Raw_Sensor = analogRead(LM35PIN);
+    float voltage = readADC_Cal(LM35_Raw_Sensor);
+    display.setCursor(34, 15);
+    display.print(String(Filter.run(voltage / 10)));
+    display.setCursor(45, 15);
+    display.print(")");
+
+    //压力传感器 食盆儿 Filter.run()
+//    int WEIGHT_Raw_Sensor = FilterOne(analogRead(WEIGHT_PIN));
+//    float new_voltage = readADC_Cal(WEIGHT_Raw_Sensor);
+//    float temp = 1000000 / (((3300 - new_voltage) * 10000) / new_voltage);
+//    float gam = 0;
+//    if (temp <= 1000) {
+//      gam = temp / 80;
+//    } else {
+//      gam = temp - 1000;
+//      gam /= 30;
+//    }
+//    Serial.print((gam * 1000) / 9.8);
+//    Serial.println();
+
+    //环境湿度
+    display.setCursor(1, 25);
+    display.print("H:");
+    display.setCursor(15, 25);
+    int h = dht.readHumidity();
+    if (h < 100) {
+      char h_char[6];
+      dtostrf(h, 2, 0, h_char);
+      display.print(h_char);
+    } else {
+      display.print("--");
+    }
+
+    //总里程
+    display.setCursor(1, 35);
     display.print("ODO:");
-    display.setCursor(30,25);
-    //运动速度相关显示
+    display.setCursor(30, 35);
     char totalRun_char[6];
-    if(totalRun>999){
-    //超过999米变更为km单位
-      dtostrf(totalRun/1000, 1, 3, totalRun_char);
+    if (totalRun > 999) {
+      //超过999米变更为km单位
+      dtostrf(totalRun / 1000, 1, 3, totalRun_char);
       strcat(totalRun_char, " km");
       display.print(totalRun_char);
-    }else{
+    } else {
       dtostrf(totalRun, 1, 1, totalRun_char);
       strcat(totalRun_char, " m");
       display.print(totalRun_char);
     }
-    
-    //实时速度
-    display.setCursor(1, 46);
-    display.print("Speed:");
-    display.setCursor(40, 46);
-    char currSpeed_char[5];
-    dtostrf(singleSpeed, 1, 1, currSpeed_char);
-    strcat(currSpeed_char, " m/s");
-    display.print(currSpeed_char);
-  
-    display.setCursor(1, 54);
+
+    //总圈数显示
+    display.setCursor(1, 50);
     display.print("lapCount:");
-    display.setCursor(55, 54);
+    display.setCursor(55, 50);
     display.print(lapCount);
 
     display.display();//屏幕显示
-    
-    vTaskDelay(1000);
+
+    vTaskDelay(2000);
   }
-   vTaskDelete(NULL);
+  vTaskDelete(NULL);
 }
 
-//每2分钟执行
-void taskTwo(void *parameter){
-  while(1){
+//每2分钟执行 mqtt传输跑圈数据
+void taskTwo(void *parameter) {
+  while (1) {
     //30s 前总圈数
     int currLapCount = lapCount;
     //等待一分钟
-    vTaskDelay(1000*60*2);
+    vTaskDelay(1000 * 60 * 2);
     //如果一分钟后总圈数大于一分钟前
-    if(currLapCount < lapCount){
+    if (currLapCount < lapCount) {
       //通过mqtt 发送总圈数到服务器
       String payload = "{\"msg\":[1,#lapCount]}";
-      payload.replace("#lapCount",String(lapCount));
-      int len= payload.length()+1;
+      payload.replace("#lapCount", String(lapCount));
+      int len = payload.length() + 1;
       char char_array[len];
-      payload.toCharArray(char_array,len);
-      client.publish("t_active",char_array);
+      payload.toCharArray(char_array, len);
+      client.publish("t_active", char_array);
     }
   }
   vTaskDelete(NULL);
@@ -221,7 +244,7 @@ void taskTwo(void *parameter){
 //主函数初始化
 void setup()
 {
-  
+
   Serial.begin(115200);
   //喂食继电器引脚
   pinMode(FEED_ONE, OUTPUT);
@@ -229,13 +252,13 @@ void setup()
 
   digitalWrite(FEED_ONE, HIGH);
   digitalWrite(FEED_TWO, HIGH);
-  
+
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -250,35 +273,35 @@ void setup()
   dht.begin();
 
   servo1.attach(SERVOPIN, 500, 2400);
-  
+
   //跑圈红外传感器引脚
   pinMode(SpeadPonit, INPUT);
 
   //从服务器获取 初始化 lapCount
   WiFiClientSecure *client = new WiFiClientSecure;
-  if(client) {
+  if (client) {
     HTTPClient https;
     client -> setCACert(rootCACertificate);
     {
-      HTTPClient https;
-      if (https.begin(*client, web_server+"/getMaxLapCount?hamsterId=1")) {
-        int httpCode = https.GET();
-        if (httpCode > 0) {
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = https.getString();
-            StaticJsonDocument<128> doc;
-            deserializeJson(doc, payload);
-            lapCount = doc["lapCount"];
-            totalRun = lapCount * Perimeter;
-          }
+      HTTPClient http;
+      http.begin(web_server + "/getMaxLapCount?hamsterId=1",rootCACertificate); 
+      int httpCode = http.GET();
+      Serial.print("response coede:");
+      Serial.println(httpCode);
+      if (httpCode > 0) {
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          StaticJsonDocument<128> doc;
+          deserializeJson(doc, payload);
+          lapCount = doc["lapCount"];
+          totalRun = lapCount * Perimeter;
         }
-      }else {
-        Serial.printf("[HTTPS] Unable to connect\n");
       }
+     
     }
     delete client;
   }
-  
+
   //子任务设定
   xTaskCreatePinnedToCore(
     taskOne,   /* Task function. */
@@ -288,7 +311,7 @@ void setup()
     1,         /* Priority of the task. */
     NULL,      /* Task handle. */
     0          /* Task Core. */
-    );  
+  );
   //子任务设定
   xTaskCreatePinnedToCore(
     taskTwo,   /* Task function. */
@@ -298,30 +321,28 @@ void setup()
     1,         /* Priority of the task. */
     NULL,      /* Task handle. */
     1          /* Task Core. */
-    );
+  );
 }
 
 void loop()
 {
-    int currSts = digitalRead(SpeadPonit);
+  int currSts = digitalRead(SpeadPonit);
 
-//    Serial.print("参数：");
-//    Serial.println(currSts);
+  //    Serial.println(currSts);
 
-    //一圈识别开始
-    if (lastSts == 1 && currSts == 0) {
-      tempTime1 = millis();//用于识别区超时
-      if (calSpeedFlg == false) {
-        calSpeedFlg = true; //在这里 开启calSpeedFlg Flg
-        singleLapCount = 0; //单次运动圈数，从开启flg开始置为0
-        singleSpeed = 0.0;
-      }
-    }
+  //一圈识别开始
+  if (lastSts == 1 && currSts == 0) {
+    tempTime1 = millis();//用于识别区超时
+  }
 
-     //一圈识别结束
+  //一圈识别结束
   if (lastSts == 0 && currSts == 1 ) {
-    float tempTime2 = millis();//用于识别区超时，理论上状态跃迁是[1,0][0,0][0,0][0,1]
-    
+    //用于识别区超时，理论上状态跃迁是[1,1][1,0][0,0][0,0][0,1][1,1]
+    //                                 |              |
+    //                                 ----------------
+    //                                 tempTime2- tempTime1
+    float tempTime2 = millis();
+
     //超时条件，如果一直处于识别区域超过1秒，则重置lastSts
     //强制转换为[1,1]状态进入到平均速度结算
     if ((tempTime2 - tempTime1) > 1000 ) {
@@ -330,38 +351,14 @@ void loop()
       //未超时，总圈数增加
       lapCount++;
       totalRun = lapCount * Perimeter;
-      singleLapCount++;
-      if (calSpeedFlg == true){
-        if(singleLapCount > 1) {//第一圈速度不准，忽略
-          float tempT = millis();
-          float useTime = (tempT - notRunTime) / 1000.0;
-          singleSpeed = Perimeter / useTime;
-        }
-      }
-    }
-    notRunTime = millis();//每次完成一圈识别的下一个状态是[1,1],刷新notRunTime,用于计算平均速度
-  }
-
-  //运动结束变量清零
-  if (lastSts == 1 && currSts == 1) {
-    if (calSpeedFlg == true) {
-      endTime = millis();
-      if ((endTime - notRunTime) > 3000) {
-        //大于3秒间隔，识别为一次连贯运动，可以进行平均速度计算了
-        calSpeedFlg = false;//关闭计算平均速度flg
-        singleSpeed = 0.0; //运动结束时 将实时速度清零
-        endTime =0.0;
-        notRunTime = 0.0;
-        singleLapCount = 0;
-      }
     }
   }
 
   //最后更新lastSts
   lastSts = currSts;
-    
-//  Serial.print("内存：");
-//  Serial.println(ESP.getFreeHeap());
+
+  //  Serial.print("内存：");
+  //  Serial.println(ESP.getFreeHeap());
 
   delay(10);
 }
